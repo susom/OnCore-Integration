145363aea97d173be2286654c0b4a9ba0c7804ae
ihabz <ihab.zeedia@stanford.edu>
2022-02-22 12:29:52 -0800

    add new entity table to save oncore admins.

--- pages/test.php
+++ pages/test.php
@@ -4,6 +4,23 @@ namespace Stanford\OnCoreIntegration;
 
 /** @var \Stanford\OnCoreIntegration\OnCoreIntegration $module */
 
-$module->onCoreProtocolsScanCron();
+// test cron
+#$module->onCoreProtocolsScanCron();
 
+// test contact functionality.
+$user = $module->framework->getUser();
+$admin = $module->getUsers()->getOnCoreAdmin($user->getUsername());
+if (!$admin) {
+    $contact = $module->getUsers()->searchOnCoreContactsViaEmail($user->getEmail());
+
+    echo '<pre>';
+    print_r($contact);
+    echo '</pre>';
+    $module->getUsers()->createOnCoreAdminEntityRecord($contact['contactId'], $user->getUsername());
+} else {
+    echo '<pre>';
+    print_r($admin);
+    //$module->getUsers()->updateOnCoreAdminEntityRecord($user->getUsername(), time(), time());
+    echo '</pre>';
+}
 
