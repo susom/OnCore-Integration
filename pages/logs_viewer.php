6a0fd2712b1115adae7ba4ff25844f8a1993d24d
ihabz <ihab.zeedia@stanford.edu>
2022-03-17 12:58:33 -0700

    cleanup.

--- pages/logs_viewer.php
+++ pages/logs_viewer.php
@@ -8,7 +8,8 @@ use REDCapEntity\EntityList;
 /** @var \Stanford\OnCoreIntegration\OnCoreIntegration $module */
 
 try {
-    if (!defined('SUPER_USER') && SUPER_USER != '1') {
+    if (!defined('SUPER_USER') || SUPER_USER != '1') {
+        Entities::createException(USERID . ' is trying to access OnCore logs');
         throw new \Exception('Access Denied');
     }
     $list = new EntityList(OnCoreIntegration::ONCORE_REDCAP_API_ACTIONS_LOG, $module);
