cda018a2fb246bd4635e48b1e4eedc7b4b29ebf1
ihabz <ihab.zeedia@stanford.edu>
2022-03-17 12:41:21 -0700

    add new page to view oncore logs.

---
+++ pages/logs_viewer.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Stanford\OnCoreIntegration;
+
+use REDCapEntity\EntityList;
+
+/** @var \Stanford\OnCoreIntegration\OnCoreIntegration $module */
+
+try {
+    if (!defined('SUPER_USER') && SUPER_USER != '1') {
+        throw new \Exception('Access Denied');
+    }
+    $list = new EntityList(OnCoreIntegration::ONCORE_REDCAP_API_ACTIONS_LOG, $module);
+    $list->render('project'); // Context: project.
+
+} catch (\Exception $e) {
+    echo '<div class="alert alert-danger">' . $e->getMessage() . '</div>';
+}
