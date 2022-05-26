2c4965deddb765a3f8fcfbcc3942d70282a70b3c
ihabz <ihab.zeedia@stanford.edu>
2022-05-26 16:56:33 -0700

    add psalm

---
+++ vendor/felixfbecker/language-server-protocol/src/DocumentOnTypeFormattingClientCapabilities.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LanguageServerProtocol;
+
+class DocumentOnTypeFormattingClientCapabilities
+{
+
+    /**
+     * Whether text document synchronization supports dynamic registration.
+     *
+     * @var bool|null
+     */
+    public $dynamicRegistration;
+
+    public function __construct(
+        bool $dynamicRegistration = null
+    )
+    {
+        $this->dynamicRegistration = $dynamicRegistration;
+    }
+}
---
+++ vendor/felixfbecker/language-server-protocol/src/ExecuteCommandClientCapabilities.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LanguageServerProtocol;
+
+class ExecuteCommandClientCapabilities
+{
+
+    /**
+     * Execute command supports dynamic registration.
+     *
+     * @var bool|null
+     */
+    public $dynamicRegistration;
+
+    public function __construct(
+        bool $dynamicRegistration = null
+    )
+    {
+        $this->dynamicRegistration = $dynamicRegistration;
+    }
+}
