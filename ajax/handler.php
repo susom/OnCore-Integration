f9311b2b78cf6287359cf0e7487468ce3e8fffac
ihabz <ihab.zeedia@stanford.edu>
2022-06-13 10:51:44 -0700

    fix json encode.

--- ajax/handler.php
+++ ajax/handler.php
@@ -278,14 +278,14 @@ try {
         $responseBodyAsString['id'] = $id;
     }
 //    echo(json_encode($responseBodyAsString, JSON_THROW_ON_ERROR));
-    echo htmlentities(json_encode($responseBodyAsString, JSON_THROW_ON_ERROR), ENT_NOQUOTES);
+    echo json_encode($responseBodyAsString, JSON_THROW_ON_ERROR);
 } catch (\Exception $e) {
     Entities::createException($e->getMessage());
     header("Content-type: application/json");
     http_response_code(404);
-    $result = json_encode(array('status' => 'error', 'message' => $e->getMessage(), 'id' => $id));
+    $result = json_encode(array('status' => 'error', 'message' => $e->getMessage(), 'id' => $id), JSON_THROW_ON_ERROR);
 //    echo(json_encode($result, JSON_THROW_ON_ERROR));
-    echo htmlentities(json_encode($result, JSON_THROW_ON_ERROR), ENT_NOQUOTES);
+    echo $result;
 }
 
 
